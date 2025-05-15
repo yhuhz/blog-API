cb8a0d995d41c6a1a0cffab59372eee718f47b97
julius.albert.grajo <julius.albert.grajo@gmail.com>
2025-05-15 16:14:25 +0800

    Added isAdmin in model

--- models/User.js
+++ models/User.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is Required'],
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
